b0848ad63c02f671ef34bfb3ea7927a4b0759ad0
CristianLopes <cristianqibins@hotmail.com>
2024-09-17 00:20:49 -0300

    add home structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        {/* Input data */}
+        <FormContainer>
+          <label htmlFor='task'>Vou trabalhar em</label>
+          <input id='task' />
+
+          <label htmlFor='minutesAmount'>durante</label>
+          <input id='minutesAmount' />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        {/* CountDown */}
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        {/* Button */}
+
+        <button>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
