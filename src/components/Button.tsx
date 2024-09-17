0ea889f67cec8e01d743a4bc274a038ddfe215bf
CristianLopes <cristianqibins@hotmail.com>
2024-09-16 23:47:32 -0300

    add default and header layout

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonVariant, StyledButton } from "./Buttom.styles";
-
-interface ButtonProps {
-  variant: ButtonVariant
-}
-
-export function Button({variant = 'primary' } : ButtonProps) {
-  return <StyledButton variant={variant}>Enviar</StyledButton>
-}
