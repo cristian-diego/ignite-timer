75077df8c4ef31ef5c7778a1dc37ca018d0e4d43
CristianLopes <cristianqibins@hotmail.com>
2024-09-17 23:12:32 -0300

    add ui for historic

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,69 @@
+import { HistoryContainer, HistoryTableContainer, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryTableContainer>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>
+                <Status statusColor='green'>Concluído</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>
+                <Status statusColor='yellow'>Em andamento</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>10 minutos</td>
+              <td>Há cerca de 2 dias</td>
+              <td>
+                <Status statusColor='red'>Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryTableContainer>
+    </HistoryContainer>
+  )
 }
